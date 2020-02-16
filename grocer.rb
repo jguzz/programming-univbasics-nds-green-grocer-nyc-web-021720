385cbeff786c137747f34aa061fa5ab5236e6e32
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-16 17:11:55 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -29,7 +29,7 @@ def consolidate_cart(cart)
       new_cart_item = {
         :item => cart[counter][:item],
         :price => cart[counter][:price],
-        :clearence => cart[counter][:clearence],
+        :clearance => cart[counter][:clearance],
         :count => 1
       }
       new_cart << new_cart_item
